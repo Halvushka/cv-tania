b2544ec86d7bb74fb781576173c8b9f46f8855d3
Marsikwd <108618678+Marsikwd@users.noreply.github.com>
2023-06-27 16:57:17 +0300

    firtst-commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+const textElement = document.querySelector('.anim-text');
+const text = 'Designer';
+let charIndex = 0;
+let isAnimating = true;
+
+function typeText() {
+  if (charIndex < text.length) {
+    textElement.textContent += text.charAt(charIndex);
+    charIndex++;
+    textElement.classList.remove('caret-hidden'); 
+    setTimeout(typeText, 100);
+  } else {
+    isAnimating = false;
+    textElement.classList.add('caret-hidden'); 
+    setTimeout(reverseText, 4000);
+  }
+}
+
+function reverseText() {
+  if (textElement.textContent.length > 0) {
+    textElement.textContent = textElement.textContent.slice(0, -1);
+    setTimeout(reverseText, 100);
+  } else {
+    isAnimating = true;
+    charIndex = 0;
+    textElement.classList.remove('caret-hidden'); 
+    setTimeout(typeText, 500); 
+  }
+}
+
+function blinkCaret() {
+  textElement.classList.toggle('caret-hidden');
+  setTimeout(blinkCaret, 500);
+}
+
+function animateText() {
+  typeText();
+  blinkCaret();
+}
+
+animateText();
+
+//----//
+
+var lazyLoadElements = document.querySelectorAll(".lazy-load");
+
+var lazyLoadCallback = function(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      var lazyLoadElement = entry.target;
+      lazyLoadElement.src = lazyLoadElement.dataset.src;
+      lazyLoadElement.classList.remove("lazy-load");
+      observer.unobserve(lazyLoadElement);
+    }
+  });
+};
+var lazyLoadObserver = new IntersectionObserver(lazyLoadCallback, {
+  root: null, 
+  rootMargin: "0px", 
+  threshold: 0.1  
+});
+
+lazyLoadElements.forEach(function(lazyLoadElement) {
+  lazyLoadObserver.observe(lazyLoadElement);
+});
